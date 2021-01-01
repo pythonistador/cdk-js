8b53d8d58ebb4a5b133ddfb4f3e3e1485f672113
pythonistador <34214896+pythonistador@users.noreply.github.com>
2021-01-01 11:57:50 -0500

    Create hello-cdk-stack.js
    added cdk-stack.js

---
+++ hello-cdk-stack.js
@@ -0,0 +1,21 @@
+const cdk = require('@aws-cdk/core');
+const s3 = require('@aws-cdk/aws-s3');
+
+
+class HelloCdkStack extends cdk.Stack {
+  /**
+   *
+   * @param {cdk.Construct} scope
+   * @param {string} id
+   * @param {cdk.StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new s3.Bucket(this, 'MyFirstBucket', {versioned: true});
+
+  }
+}
+
+module.exports = { HelloCdkStack }
